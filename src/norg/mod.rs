c243c26b2f9c657c0c56cf5c1fa1c1df7ba267c4
Seongmin Lee <boltlessengineer@gmail.com>
2023-12-17 07:15:57 +0000

    chore: cleanup cmp generation function

--- src/norg/mod.rs
+++ src/norg/mod.rs
@@ -8,44 +8,33 @@ pub struct NorgCompletion {
     pub kind: CompletionItemKind,
     pub text: String,
     pub desc: String,
+    // HACK: find better way to check validity
     /// valid parent node types
     pub valid_parents: Vec<String>,
 }
 
-impl Into<NorgCompletion> for (&str, CompletionItemKind, &str, &str) {
-    fn into(self) -> NorgCompletion {
-        NorgCompletion {
-            name: String::from(self.0),
-            kind: self.1,
-            text: String::from(self.2.trim()),
-            desc: String::from(self.3),
-            valid_parents: vec![],
-        }
+fn make_cmp_item(name: &str, kind: CompletionItemKind, text: &str, desc: &str) -> NorgCompletion {
+    NorgCompletion {
+        name: String::from(name),
+        kind,
+        text: String::from(text.trim()),
+        desc: String::from(desc),
+        valid_parents: vec![],
     }
 }
 
 pub static NORG_BLOCKS: OnceLock<Vec<NorgCompletion>> = OnceLock::new();
 
-// "@" -> all types of verbatim ranged tags
-// "|" -> all types of standard ranged tags
-// "#" -> all types of strong carryover tags
-
 pub fn init_norg_completion() {
     NORG_BLOCKS
         .set(vec![
-            (
+            make_cmp_item(
                 "code",
                 CompletionItemKind::SNIPPET,
                 include_str!("./blocks/code_text.txt"),
                 include_str!("./blocks/code_desc.md"),
-            )
-                .into(),
-            // (
-            //     "code",
-            //     include_str!("./blocks/code_text.txt"),
-            //     include_str!("./blocks/code_desc.md"),
-            // )
-            //     .into(),
+            ),
+            // TODO: add more
         ])
         .unwrap();
 }
