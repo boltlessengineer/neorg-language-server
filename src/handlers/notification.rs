8bdf3a47b2e862ec9c80e99e01c0f106ddb2a328
Seongmin Lee <boltlessengineer@gmail.com>
2023-12-23 06:17:46 +0000

    chore: remove unused logs

--- src/handlers/notification.rs
+++ src/handlers/notification.rs
@@ -25,17 +25,12 @@ pub fn handle_did_change(params: serde_json::Value) {
         let mut doc_store = DOC_STORE.get().unwrap().lock().unwrap();
         let uri = text_document.uri.to_string();
         let doc = doc_store.get_mut(&uri).expect("can't find document");
-        error!("udpate text document");
         changes.iter().for_each(|change| {
             if let Some(range) = change.range {
                 doc.change_range(range, &change.text);
             }
         });
-        error!("doc:");
-        error!("{}", doc.text.to_string());
         doc.update();
-        error!("tree:");
-        error!("{}", doc.tree.root_node().to_sexp());
     }
 }
 
